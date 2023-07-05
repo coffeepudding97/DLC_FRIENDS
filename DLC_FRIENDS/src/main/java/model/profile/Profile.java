fa00c696b33590647b77d77b2d9a9f8fb234648f
yun <dldbswjd7879@gmail.com>
2023-07-05 17:14:31 +0900

    file add #3

--- DLC_FRIENDS/src/main/java/model/profile/Profile.java
+++ DLC_FRIENDS/src/main/java/model/profile/Profile.java
@@ -1,5 +1,31 @@
 package model.profile;
 
 public class Profile {
+	private String id;
+	private String nickname;
+	private String intro;
+	
+	public Profile(String id, String nickname, String intro) {
+		super();
+		this.id = id;
+		this.nickname = nickname;
+		this.intro = intro;
+	}
 
+	public String getId() {
+		return id;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public String getIntro() {
+		return intro;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("id: %s\nnickname: %s\nintro: %s", this.id, this.nickname, this.intro);
+	}
 }
