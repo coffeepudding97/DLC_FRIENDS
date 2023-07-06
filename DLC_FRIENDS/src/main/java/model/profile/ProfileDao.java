f7e58e6c59fff1e02007e29e362822e2b31d03d2
yun <dldbswjd7879@gmail.com>
2023-07-06 18:05:32 +0900

    ImageBlob생성,MemberInfoModifyAction생성,Profile수정,profile.js생성,profile.jsp수정,profiledao수정,profiledto수정,profileformaction수정,testInput.jsp생성(테스트용),web.xml수정

--- DLC_FRIENDS/src/main/java/model/profile/ProfileDao.java
+++ DLC_FRIENDS/src/main/java/model/profile/ProfileDao.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
+import java.sql.Blob;
+
 import util.DBManager;
 
 public class ProfileDao {
@@ -23,6 +25,46 @@ public class ProfileDao {
 		return instance;
 	}
 	
+//	public Profile getAllProfiles() {
+//		Profile profile = null;
+//		
+//		this.conn = DBManager.getConnection();
+//		
+//		if(this.conn != null) {
+//			String sql = "SELECT * FROM profile";
+//			try {
+//				if(this.rs.next()) {
+//					String id = this.rs.getString(1);
+//					String nickname = this.rs.getString(2);
+//					String info = this.rs.getString(3);
+//					System.out.println(id + nickname + info);
+//					profile = new Profile(id, nickname, info);
+//				}
+//				
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			} finally {
+//				try {
+//					this.pstmt.close();
+//					conn.close();
+//				}catch(Exception e){
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+//				
+//		
+//		return profile;
+//	}
+	
+	
+	/*
+		SELECT profile.*, post.title, post.content
+		FROM profile
+		JOIN post ON profile.user_id = post.user_id
+		WHERE profile.user_id = 'dlrbwo2023';
+		
+	 */
 	// 프로필 불러오기(id 받아오고 profile 정보 반환)
 	public Profile getUserProfile(String id) {
 		Profile profile = null;
@@ -30,19 +72,27 @@ public class ProfileDao {
 		this.conn = DBManager.getConnection();
 		
 		if(this.conn != null) {
-			String sql = "SELECT * FROM profile WHERE id=?";
+			String sql = "SELECT * FROM profile WHERE user_id=?";
 			
 			try {
 				this.pstmt = this.conn.prepareStatement(sql);
+				// profile_no, user_id, profile_img, info
 				this.pstmt.setString(1, id);
 				
 				this.rs = this.pstmt.executeQuery();
 				
 				if(this.rs.next()) {
-					String nickname = this.rs.getString(2);
-					String info = this.rs.getString(3);
+					Blob profileImg = this.rs.getBlob(3);
+					String info = this.rs.getString(4);
 					
-					profile = new Profile(id, nickname, info);
+//					if(title.equals(null)) {
+//						title = "";
+//					}
+//					if(content.equals(null)) {
+//						content = "";
+//					}
+					
+					profile = new Profile(id, profileImg, info);
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -57,4 +107,6 @@ public class ProfileDao {
 		}
 		return profile;
 	}
+	
+	
 }
